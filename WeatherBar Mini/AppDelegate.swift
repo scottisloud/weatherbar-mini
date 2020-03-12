5b5aa84b4a122c70ba9d27842549d73613af27a6
Scott Lougheed <scott@scottlougheed.com>
2020-03-12 12:16:15 -0400

    Initial Commit

---
+++ WeatherBar Mini/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  WeatherBar Mini
+//
+//  Created by Scott Lougheed on 2020/3/12.
+//  Copyright © 2020 Scott Lougheed. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+	func applicationDidFinishLaunching(_ aNotification: Notification) {
+		// Insert code here to initialize your application
+	}
+
+	func applicationWillTerminate(_ aNotification: Notification) {
+		// Insert code here to tear down your application
+	}
+
+
+}
+
